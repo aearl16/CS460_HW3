facf286a07101502a8241c85557cbdc669da8043
Aaron Earl <com_tech12@yahoo.com>
2017-10-20 18:51:20 -0700

    program works but doesn't get the right calculation

---
+++ src/PostfixCalculator/Node.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostfixCalculator
+{
+    class Node
+    {
+        private static object data;
+        public static Node next;
+
+        public Node()
+        {
+            data = null;
+            next = null;
+        }
+
+        public Node(object data, Node next)
+        {
+            Node.data = data;
+            Node.next = next;
+        }
+
+        /**
+        public object Data
+        {
+            set => data = value;
+            get => data;
+        }
+        */
+
+        //More CSharpish way to do it
+        public object Data { get; set; } = data;
+
+        public Node Next { set; get; } = next;
+    }
+}
